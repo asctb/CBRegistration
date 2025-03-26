a2ab04b38d32c555c2b1b615cf38cf531829b9fb
bek17 <bek17@BATUHAN>
2025-03-26 14:27:05 +0300

    updated packages, DAL project added db contetext to DAL

---
+++ CBRegistration.Solution/CBRegistration.DAL/Contexts/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CBRegistration.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBRegistration.DAL.Contexts
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<UserEntity> Users { get; set; }
+    }
+}
